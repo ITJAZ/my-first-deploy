82a13c793f8310fbc00eb7e07103583ffb73478e
Almonds <poex0716@gmail.com>
2024-07-20 23:17:07 +0800

    adding font

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
+import { Button } from '@mui/material'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Button variant='contained'>Cool</Button>
     </>
   )
 }
